9618a5223a21884a9f557caebfbea385f176f91d
eunbee <eunbee1224@gmail.com>
2020-05-09 23:50:26 +0900

    <feature>: WIP:consuming restful webservice
    https://spring.io/guides/gs/consuming-rest/

---
+++ src/main/java/com/practice/spring/consumingrest/Quote.java
@@ -0,0 +1,18 @@
+package com.practice.spring.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+@ToString
+public class Quote {
+
+    private String type;
+    private String value;
+
+
+}
---
+++ src/main/java/com/practice/spring/consumingrest/Value.java
@@ -0,0 +1,16 @@
+package com.practice.spring.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+@ToString
+public class Value {
+
+    private Long id;
+    private String quote;
+}
