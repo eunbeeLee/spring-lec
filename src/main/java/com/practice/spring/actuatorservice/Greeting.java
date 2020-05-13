566a64e754c45d4d20190a53a7f577c93ae6fd17
eunbee <eunbee1224@gmail.com>
2020-05-13 23:37:03 +0900

    <feature>: WIP:building a restful web service with spring boot actuator
    https://spring.io/guides/gs/actuator-service/

---
+++ src/main/java/com/practice/spring/actuatorservice/Greeting.java
@@ -0,0 +1,16 @@
+package com.practice.spring.actuatorservice;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Greeting {
+    private long id;
+    private String content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+}
